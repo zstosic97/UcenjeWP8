829c37bee89c19f1f00430dfb897599d5dc27c24
zstosic97 <stosic.zlatko@gmail.com>
2025-05-15 21:21:43 +0200

    kreiranje tablica proizvodi i stavke

--- trgovina.sql
+++ trgovina.sql
@@ -7,3 +7,15 @@
 --broj varchar(10),
 --kupac varchar(100)
 --);
+
+--create table proizvodi(
+--sifra int,
+--naziv varchar(100),
+--cijena decimal(18,2)
+--);
+
+create table stavke(
+racun int,
+proizvod int,
+kolicina decimal(5,3)
+);
