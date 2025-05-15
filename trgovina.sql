9a36592d26a63f222f9f8fd4a3b1a6ac446d9ad9
zstosic97 <stosic.zlatko@gmail.com>
2025-05-15 23:03:36 +0200

    Kreiranje dijagrama trgovina

--- trgovina.sql
+++ trgovina.sql
@@ -1,21 +1,28 @@
-﻿--create database trgovina;
+﻿use master;
+go
+drop database if exists trgovina;
+go
 
---use trgovina;
+create database trgovina;
+go
 
---create table racuni(
---sifra int,
---broj varchar(10),
---kupac varchar(100)
---);
+use trgovina;
 
---create table proizvodi(
---sifra int,
---naziv varchar(100),
---cijena decimal(18,2)
---);
+create table racuni(
+sifra int not null primary key identity(1,1),
+broj varchar(10) not null,
+kupac varchar(100)
+);
 
---create table stavke(
---racun int,
---proizvod int,
---kolicina decimal(5,3)
---);
+create table proizvodi(
+sifra int not null primary key identity(1,1),
+naziv varchar(50) not null,
+cijena decimal(18,2) not null
+);
+
+create table stavke(
+racun int not null references racuni(sifra),
+proizvod int not null references proizvodi(sifra),
+kolicina decimal(5,3) not null
+);
+
