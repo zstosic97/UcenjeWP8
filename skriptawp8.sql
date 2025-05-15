7677d0031b97d4e9101f363643a51abf7d771eca
zstosic97 <stosic.zlatko@gmail.com>
2025-05-15 20:43:43 +0200

    Kreiranje tablice smjer

--- skriptawp8.sql
+++ skriptawp8.sql
@@ -1,4 +1,13 @@
-﻿create database edunovawp8;
+﻿--create database edunovawp8;
 
---drop database edunovawp8;
+-- drop database edunovawp8;
 
+--use edunovawp8;
+
+create table smjerovi(
+sifra int,
+naziv varchar(50),
+cijena decimal(18,2),
+datumpokretanja datetime,
+aktivan bit
+);
