fde9d8a4c93ac8d2c66c251b24021e49abeb4527
zstosic97 <stosic.zlatko@gmail.com>
2025-05-27 19:07:27 +0200

    nastavljam insertat

--- poljop_ap_ZStosic.sql
+++ poljop_ap_ZStosic.sql
@@ -13,7 +13,7 @@ Create table prodavatelji(
 sifra int not null primary key identity(1,1),
 naziv varchar(50) not null,
 adresasjedista varchar(50) not null,
-email varchar(50) not null,
+email varchar(50) not null
 );
 
 
@@ -26,15 +26,8 @@ create table proizvodi(
 sifra int not null primary key identity(1,1),
 nazivproizvoda varchar (30) not null,
 kolicina decimal (18,3) not null,
-vlaga decimal (3,2), 
-primjesa decimal (3,2), 
-hektolitar decimal (3,2),
-protein decimal (3,2),
-ulje decimal (3,2),
-lom decimal (3,2),
-defekt decimal (3,2),
 prodavatelj int not null references prodavatelji(sifra),
-kategorija int not null references kategorijeproizvoda(sifra),
+kategorija int not null references kategorijeproizvoda(sifra)
 );
 
 
@@ -42,7 +35,7 @@ create table kupci(
 sifra int not null primary key identity(1,1),
 naziv varchar(50) not null,
 adresasjedista varchar(50) not null,
-email varchar (50) not null,
+email varchar (50) not null
 );
 
 create table narudzbe(
@@ -57,4 +50,44 @@ kolicina decimal (18,3) not null,
 narudzba int not null references narudzbe(sifra),
 proizvod int not null references proizvodi(sifra)
 );
+
+insert into prodavatelji(naziv, adresasjedista, email)
+values 
+('OPG Horvat', 'Glavna 12, Osijek', 'horvat@opg.hr'),
+('OPG Kovač', 'Poljska 8, Varaždin', 'kovac@opg.hr'),
+('OPG Marić', 'Vinogradska 14, Vinkovci', 'maric@opg.hr'),
+('OPG Petrović', 'Zagrebačka 20, Slavonski Brod', 'petrovic@opg.hr'),
+('OPG Novak', 'Trg Mladosti 3, Karlovac', 'novak@opg.hr'),
+('OPG Radić', 'Put Žita 9, Osijek', 'radic@opg.hr'),
+('OPG Šarić', 'Lipa 22, Đakovo', 'saric@opg.hr'),
+('OPG Grgić', 'Selska 5, Požega', 'grgic@opg.hr'),
+('OPG Bašić', 'Poljoprivredna 4, Sisak', 'basic@opg.hr'),
+('OPG Vuković', 'Voćarska 11, Bjelovar', 'vukovic@opg.hr');
+
+insert into kategorijeproizvoda(nazivkategorije)
+values
+('Žitarice'),('uljarice');
+
+insert into proizvodi(nazivproizvoda, kolicina, prodavatelj, kategorija)
+values 
+('Pšenica', 1500.000, 1, 1),
+('Kukuruz', 1200.000, 2, 1),
+('Suncokret', 800.000, 3, 2),
+('Ječam', 900.000, 4, 1),
+('Soja', 700.000, 5, 2),
+('Zob', 500.000, 7, 1),
+('Uljana repica', 450.000, 8, 2);
+
+INSERT INTO kupci(naziv, adresasjedista, email)
+VALUES 
+('Podravka d.d.', 'Danica 1, Koprivnica', 'info@podravka.hr'),
+('Konzum d.d.', 'Vukovarska 269a, Zagreb', 'narudzbe@konzum.hr'),
+('Plodine d.d.', 'Kukuljanovo bb, Rijeka', 'kupovina@plodine.hr'),
+('Studenac d.o.o.', 'Zadarska 45, Split', 'prodaja@studenac.hr'),
+('Žito d.d.', 'Đakovština 3, Osijek', 'nabava@zito.hr');
+
+INSERT INTO narudzbe(datum, kupac)
+VALUES 
+('2025-05-27', 1), 
+('2025-05-28', 3);
 
