5d7a78386321ad64e7a8bbf23e846f79c70e470d
zstosic97 <stosic.zlatko@gmail.com>
2025-06-05 20:34:39 +0200

    UlazIzlaz

---
+++ BACKEND/Ucenje/E01UlazIzlaz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucenje
+{
+    internal class E01UlazIzlaz
+    {
+
+        
+        public static void Izvedi() 
+        {
+
+
+            Console.Write("Pozdrav svijetu");
+            Console.WriteLine("Isti red");
+
+            Console.WriteLine("Osijek je \n SUPER"); // \n je novi red
+
+            Console.WriteLine("\t\tOvo je uvučeno");
+
+            Console.Write("Unesi svoje ime: ");
+
+            // variabla ime 
+            string ime = Console.ReadLine();
+
+
+            Console.WriteLine("Unijeli ste {0}", ime);
+
+
+        } // kraj metode
+
+
+
+    } // kraj klase 
+} // kraj imenskog prostora (namespace)
--- BACKEND/Ucenje/Program.cs
+++ BACKEND/Ucenje/Program.cs
@@ -1,3 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Ucenje;
+
+E01UlazIzlaz.Izvedi();
+
+
+
+
+
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
 
