891dbbb354861fc0bb823a0419abf29e7e2c05fd
zstosic97 <stosic.zlatko@gmail.com>
2025-06-06 19:51:56 +0200

    Osnovni operatori

---
+++ BACKEND/Ucenje/E02Z1.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Ucenje
+{
+    internal class E02Z1
+    {
+
+        // Zadatak:
+        // Program Traži unos plaće i ispisuje ju
+
+        public static void Izvedi()
+        {
+
+            Console.WriteLine("E01Z1");
+            Console.WriteLine("Unesi iznos plaće (npr.123,98): ");
+
+            double placa = double.Parse(Console.ReadLine());
+            Console.WriteLine(placa);
+        
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ BACKEND/Ucenje/E03Operatori.cs
@@ -0,0 +1,63 @@
+﻿
+
+namespace Ucenje
+{
+    internal class E03Operatori
+    {
+
+        public static void Izvedi()
+        {
+            Console.WriteLine("E03Operatori");
+
+            // = dodjeljivanje vrijednosti s desne na lijevu stranu
+
+            // neću objašnjavati + - / * matematički
+
+            // + kao operator nadoljepljivanja (concate)
+
+
+            string grad = "Osijek";
+            string ime = "Edunova";
+
+                Console.WriteLine(grad + " " + ime + " " + 5); // ovo nije najbolja praksa
+
+            Console.WriteLine( "{0} {1} {2} ", grad, ime, 5); // odrađuje isti posao kao gornja ali brže
+
+
+            // operator modulo % -> parnosti broja
+            Console.WriteLine(8%2); //0
+
+            int b = 9 % 2;
+
+            Console.WriteLine(b);
+
+
+            // uvećanje broja za 1
+            int i = 0;
+
+            // uvećaj za 1
+            i = i + 1;
+
+            i += 1;
+
+            i++;
+
+            ++i;
+
+            // increment i decrement (++i, i++, --i, i--);
+
+            int k = 1, j = 0;
+
+            k = ++k - j++; // k = 2 - 0, j = 1
+
+            Console.WriteLine(--k+j++); //što se ispisuje 1+1=2
+
+
+
+
+
+
+        }
+        
+    }
+}
