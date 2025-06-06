5434ae287181d74bcda1a4912e13ac81deb17672
zstosic97 <stosic.zlatko@gmail.com>
2025-06-06 19:51:43 +0200

    Uvjetna grananja

---
+++ BACKEND/Ucenje/E04UvjetnoGrananjeIf.cs
@@ -0,0 +1,126 @@
+﻿
+
+using System.ComponentModel.Design;
+
+namespace Ucenje
+{
+    internal class E04UvjetnoGrananjeIf
+    {
+
+        public static void Izvedi()
+        {
+            Console.WriteLine("E04UvjetnoGrananjeIf");
+
+
+            int broj = 7;  //Ovo je unio korisnik
+
+            // operator == provjera jednakosti, rezultat je bool tip podatka
+
+            bool uvjet = broj == 7;
+
+            // osnovna if sintaksa
+            // if radi s bool tipom podatka
+
+            if (uvjet)
+            {
+                Console.WriteLine("super");
+            }
+
+            // if ne mora imati else
+            // if ne mora iumati {} ali kada ih nema odnosi se samo na prvu sljedeću liniju
+
+            // puna sintaksa if naredbe
+
+            if (broj > 2)
+            {
+                Console.WriteLine("OK");
+                    
+                    }
+            else if (broj == 1)
+            {
+                Console.WriteLine("Nije dobro");
+
+            }
+            else
+            {
+                Console.WriteLine("NIJE OCJENA");
+            }
+
+
+            // operatori <>, >=, <=, != se koriste u if-u
+
+            // logički operatori: and, or i not 
+
+            //and 
+            //& (shift+6)
+
+            int i = 5, j = 10;
+
+            // prvi uvjet ne prolazi ALI se drugi uvjet gleda
+            if(i<10 & j > 2)
+            {
+
+                Console.WriteLine("Neće se ispisati");
+            }
+
+
+            //INAČE KAKO SE KORISTI
+            // AKO prvi uvjet ne prođe, ne gleda se drugi
+            if (i < 10 && j > 2)
+            {
+
+                Console.WriteLine("Neće se ispisati");
+            }
+
+
+            //or
+            // | (altgr+w)
+            // ako prvi uvjet prođe ne gleda se drugi
+            if(i==4 || j == 2)
+            {
+                Console.WriteLine("Jedno od dvoje");
+            }
+
+
+            //not
+            // !
+
+            if (!uvjet)
+            {
+                Console.WriteLine("Kada gore ne ispiše, ovdje će");
+
+
+            }
+
+
+            string grad = "Osijek";
+
+            if (uvjet && i > 10)
+            {
+                if (grad == "Vukovar")
+                {
+                    Console.WriteLine("Novi Prvoligaš");
+                }
+
+            }
+
+
+            if(grad == "Osijek")
+            {
+                Console.WriteLine("super");
+            }
+            else
+            {
+                Console.WriteLine("OK");
+            }
+
+
+            //inline if
+
+            Console.WriteLine(grad=="Osijek" ? "Super" : "OK");
+
+        }
+
+
+    }
+}
---
+++ BACKEND/Ucenje/E05UvjetnoGrananjeSwitch.cs
@@ -0,0 +1,51 @@
+﻿
+
+namespace Ucenje
+{
+    internal class E05UvjetnoGrananjeSwitch 
+    {
+        public static void Izvedi()
+        {
+            Console.WriteLine("E05UvjetnoGrananjeSwitch");
+
+            //switch radi s tipovima podattaka: int, string, char i 
+
+            Console.WriteLine("Unesi ocjenu brojem: ");
+
+            int broj = int.Parse(Console.ReadLine());
+
+            switch (broj)
+            {
+                case 1:
+                    Console.WriteLine("Nedovoljan");
+                    break;
+                case 2:
+                    Console.WriteLine("Dovoljan");
+                    break;
+                case 3:
+                    Console.WriteLine("Dobar");
+                    break;
+                case 4:
+                    Console.WriteLine("Vrlo Dobar");
+                    break;
+                case 5:
+                    Console.WriteLine("Odličan");
+                    break;
+                case 6:
+                case 7:
+                    Console.WriteLine("Pa i to bi mogle biti ocjene ali nisu");
+                    break;
+                default:
+                    Console.WriteLine("Nije ocjena");
+                    break;
+
+
+
+
+
+            }
+
+        }
+
+    }
+}
